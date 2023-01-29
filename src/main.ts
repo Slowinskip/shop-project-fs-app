7afe5c60400ba72089e9509af7959a5fb594f900
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-01-29 11:18:20 +0100

    Add endpoint to product

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Product } from './product.interfece';
 
 @Injectable()
 export class ProductService {
   constructor(private prismaService: PrismaService) {}
+
+  public async getAll(): Promise<Product[]> {
+    return this.prismaService.product.findMany();
+  }
+
+  public getById(id: Product['id']): Promise<Product | null> {
+    return this.prismaService.product.findUnique({
+      where: { id },
+    });
+  }
 }
