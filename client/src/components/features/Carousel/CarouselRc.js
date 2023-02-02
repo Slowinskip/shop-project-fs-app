634abc73113fb6a8c2cc4070427a9a48317c29ee
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-02-02 18:18:45 +0100

    Complete html and css in carousel

--- client/src/components/features/Carousel/CarouselRc.js
+++ client/src/components/features/Carousel/CarouselRc.js
@@ -1,3 +1,4 @@
+import Button from 'react-bootstrap/Button';
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import styles from './CarouselRc.module.scss';
@@ -5,46 +6,59 @@ import styles from './CarouselRc.module.scss';
 const CarouselRc = () => {
   return (
     <Carousel fade className={styles.carousel}>
-      <Carousel.Item>
+      <Carousel.Item className={styles.carouselItem}>
         <img
           className="d-block w-100 "
           src=".\images\carousel\nike1.jpg"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <div className={styles.content}>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <Carousel.Caption className={styles.content}>
+          <div>
+            <h3>Meet Nike</h3>
+            <div className="d-flex justify-content-center align-items-center ">
+              <p className="px-3 m-0 ">Convenience comes first </p>
+              <Button className={styles.btn} variant="light">
+                See more
+              </Button>
+            </div>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item className={styles.carouselItem}>
         <img
           className="d-block w-100"
           src=".\images\carousel\adidas1.jpg"
           alt="Second slide"
         />
 
-        <Carousel.Caption>
-          <div className={styles.content}>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <Carousel.Caption className={styles.content}>
+          <div>
+            <h3>Runners Choices</h3>
+            <div className="d-flex justify-content-center align-items-center ">
+              <p className="px-3 m-0 ">Now up to -30%</p>
+              <Button className={styles.btn} variant="light">
+                See more
+              </Button>
+            </div>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item className={styles.carouselItem}>
         <img
           className="d-block w-100"
           src=".\images\carousel\nike-woman.jpg"
           alt="Third slide"
         />
 
-        <Carousel.Caption>
-          <div className={styles.content}>
+        <Carousel.Caption className={styles.content}>
+          <div>
             <h3>Just for woman!</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <div className="d-flex justify-content-center align-items-center ">
+              <p className="px-3 m-0 ">Rose never gets bored</p>
+              <Button className={styles.btn} variant="light">
+                See more
+              </Button>
+            </div>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
