1e87386bf99c140a719f0c9baa1de481ccd46e65
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-02-03 18:59:15 +0100

    Create seed.ts and push data to server

--- client/src/components/features/FeaturesProduct/FeatureProduct.js
+++ client/src/components/features/FeaturesProduct/FeatureProduct.js
@@ -2,6 +2,10 @@ import React from 'react';
 import styles from './FeatureProduct.module.scss';
 
 const FeatureProduct = () => {
+  fetch('http://localhost:5000/api/product')
+    .then((res) => res.json())
+    .then((tables) => console.log(tables));
+
   return <div>FeatureProduct</div>;
 };
 
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,20 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import cardReducer from './cardRedux';
 import initialState from './initialState';
+import productsReducer from './productsRedux';
+import userReducer from './userRedux';
 
-const subreducers = {};
+const subreducers = {
+  products: productsReducer,
+  user: userReducer,
+  card: cardReducer,
+};
 
 const reducer = combineReducers(subreducers);
 const store = createStore(
   reducer,
   initialState,
-
   compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__
