24b70494b45622c425c8f1dc82894a26045697a1
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-02-09 18:59:40 +0100

    Add component Quantity, complete html and css

--- client/src/components/common/Quantity/Quantity.js
+++ client/src/components/common/Quantity/Quantity.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import styles from './Quantity.module.scss';
 
 const Quantity = () => {
-  return <div>Quantity</div>;
+  return (
+    <div className="d-flex flex-row">
+      <h4>Quantity:</h4>
+      <div className={'d-flex ' + styles.quantifiDiv}>
+        <Button variant="outline-dark">-</Button>
+        <input
+          type="text"
+          min={1}
+          max={10}
+          value={1}
+          readOnly
+          className={'text-center ' + styles.input}
+        />
+        <Button variant="outline-dark">+</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Quantity;
--- client/src/components/layout/Home/Home.js
+++ client/src/components/layout/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Quantity from '../../common/Quantity/Quantity';
 import CarouselRc from '../../features/Carousel/CarouselRc';
 import FeatureProduct from '../../features/FeaturesProduct/FeatureProduct';
 import ImageBox from '../../features/ImageBox/ImageBox';
@@ -6,6 +7,7 @@ import ImageBox from '../../features/ImageBox/ImageBox';
 const Home = () => {
   return (
     <>
+      <Quantity />
       <CarouselRc />
       <ImageBox />
       <FeatureProduct />
