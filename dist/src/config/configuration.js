7a2b4f58a112bc39ee9028543f17dc34293781c4
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-02-14 13:16:29 +0100

    Upload packege.json

--- dist/src/config/configuration.js
+++ dist/src/config/configuration.js
@@ -4,7 +4,7 @@ exports.default = () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
     jwt: {
         secret: process.env.JWT_SECRET,
-        expiresIn: '12h',
+        expiresIn: '11h',
     },
 });
 //# sourceMappingURL=configuration.js.map
